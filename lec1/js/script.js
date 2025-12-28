b5915ad257ae745271f76737a378387c3b897e90
Mohammed Naji <m.naji1512@gmail.com>
2025-12-28 11:50:52 +0200

    introduction and variables

---
+++ lec1/js/script.js
@@ -0,0 +1,62 @@
+function printMyName() {
+  alert(555);
+}
+
+// console.log("Mohammed");
+// console.log(55);
+// var , let , const
+// var age = 30;
+// age = 40;
+// console.log(age);
+
+// var x = 20;
+
+// if (true) {
+//   var x = 30;
+// }
+
+// console.log(x);
+
+// let age = 20;
+// age = 40;
+// console.log(age);
+
+// let x = 20;
+
+// if (true) {
+//   let x = 40;
+// }
+
+// console.log(x);
+
+// const age = 10;
+// console.log(age);
+
+// _name = "Mohammed";
+// console.log(_name);
+
+// const user = {
+//   name: "Mohammed",
+//   age: 30,
+// };
+
+// // user.name = "Zina";
+// user = {
+//   name: "Zina",
+//   age: 4,
+// };
+// console.log(user);
+
+let myName = 10;
+console.log(typeof myName);
+
+// camelCase => myName
+// PascalCase => MyName
+// snake_case => my_name
+// kebab-case => my-name
+// ===
+// !=
+// <=
+// >=
+// =>
+//   ->
